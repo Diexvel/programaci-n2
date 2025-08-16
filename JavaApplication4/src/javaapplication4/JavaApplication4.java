1e4471743e0e55117e49bcda1b5aa8f6713c48b4
DiexVel <velardesdiego@gmail.com>
2025-08-16 18:52:55 -0300

    Primer commit TP1

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Diego";
+        int edad = 35;
+        double altura = 1.78;
+        boolean estudiante = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estudiante: " + estudiante);
+    }
+    
+}
